2dc5576d48be3db11703c8a5cfba1f9a58049dff
yanan.wu <wuyn1760@aliyun.com>
2019-05-12 09:05:59 +0800

    20190512_决策树

--- simple/classifier.py
+++ simple/classifier.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from sklearn import tree
+from sklearn.datasets import load_iris
+import numpy as np
 
 
 def classifier():
@@ -10,4 +12,21 @@ def classifier():
     clf = clf.fit(features, lables)
     print(clf.predict([[150, 0]]))
 
-classifier()
+
+def tree_decision():
+    iris = load_iris()
+    test_idx = [0, 50, 100]
+    train_target = np.delete(iris.target, test_idx)
+    train_data = np.delete(iris.data, test_idx, axis=0)
+
+    test_target = iris.target[test_idx]
+    test_data = iris.data[test_idx]
+
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(train_data, train_target)
+
+    print(test_target)
+    print(clf.predict(test_data))
+
+
+tree_decision()
