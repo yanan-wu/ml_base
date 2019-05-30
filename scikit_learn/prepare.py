f64f50842f0bd32ca9713530b2b43fc6fb72bc4e
yanan.wu <wuyn1760@aliyun.com>
2019-05-31 07:39:02 +0800

    2018053101_regression

--- scikit_learn/prepare.py
+++ scikit_learn/prepare.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt # plt 用于显示图片
 import matplotlib.image as mpimg # mpimg 用于读取图片
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn import neighbors
+from sklearn.linear_model import LinearRegression
 
 
 def img():
@@ -43,4 +45,41 @@ def show():
     plt.show()
 
 
-show()
+def knn():
+    iris = load_iris()
+    X, y = iris.data, iris.target
+
+    # create the model
+    knn = neighbors.KNeighborsClassifier(n_neighbors=5, weights='uniform')
+
+    # fit the model
+    knn.fit(X, y)
+
+    # What kind of iris has 3cm x 5cm sepal and 4cm x 2cm petal?
+    X_pred = [3, 5, 4, 2]
+    result = knn.predict([X_pred, ])
+
+    print(iris.target_names[result])
+    print(iris.target_names)
+    print(knn.predict_proba([X_pred, ]))
+
+
+def linear_regression():
+    np.random.seed(0)
+    X = np.random.random(size=(20, 1))
+    y = 3 * X.squeeze() + 2 + np.random.randn(20)
+    plt.plot(X.squeeze(), y, 'o')
+    plt.show()
+
+    model = LinearRegression()
+    model.fit(X, y)
+    # Plot the data and the model prediction
+    X_fit = np.linspace(0, 1, 100)[:, np.newaxis]
+    y_fit = model.predict(X_fit)
+
+    plt.plot(X.squeeze(), y, 'o')
+    plt.plot(X_fit.squeeze(), y_fit)
+    plt.show()
+
+
+linear_regression()
