3d7b2ab40243d9ed3b321f1d6aa87436db851974
yanan.wu <wuyn1760@aliyun.com>
2019-06-29 08:03:34 +0800

    2018062801_regression

--- ml_tutorial/regression.py
+++ ml_tutorial/regression.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 import quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 
 def get_date_main():
@@ -23,18 +24,43 @@ def get_date_main():
     forecast_out = int(math.ceil(0.01*len(df)))
     print(forecast_out)
     df['label'] = df[forecast_col].shift(-forecast_out)
-    df.dropna(inplace=True)
 
     X = np.array(df.drop(['label'], 1))
-    y = np.array(df['label'])
     X = preprocessing.scale(X)
+    X = X[:-forecast_out]
+    X_lately = X[-forecast_out:]
+
+    df.dropna(inplace=True)
     y = np.array(df['label'])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     clf = LinearRegression()
     clf.fit(X_train, y_train)
     accuracy = clf.score(X_test, y_test)
-    print(accuracy)
+    # print(accuracy)
+
+    forecast_set = clf.predict(X_lately)
+    print(forecast_set, accuracy, forecast_out)
+    df['Forecast'] = np.nan
+
+    style.use('ggplot')
+    # 这里不懂取的什么
+    last_date = df.iloc[-1].name
+    last_unix = last_date.timestamp()
+    one_day = 8640
+    next_unix = last_unix + one_day
+
+    for i in forecast_set:
+        next_date = datetime.datetime.fromtimestamp(next_unix)
+        next_unix += one_day
+        df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+    df['Adj. Close'].plot()
+    df['Forecast'].plot()
+    plt.legend(loc=4)
+    plt.xlabel('Date')
+    plt.ylabel('price')
+    plt.show()
 
 
 get_date_main()
