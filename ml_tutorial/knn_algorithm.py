9dd90949f78120b50fbc3dafc99f822d8bf422f9
yanan-wu <wuyn1760@aliyun.com>
2019-07-21 09:05:03 +0800

    2019.7.21_knn

--- ml_tutorial/knn_algorithm.py
+++ ml_tutorial/knn_algorithm.py
@@ -14,15 +14,25 @@ def prepare_dataset():
     style.use('fivethirtyeight')
     dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
     new_features = [5, 7]
-    [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-    plt.show()
+    k_nearest_neighbors(dataset, new_features, 2)
+    # [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+    # plt.show()
 
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('k is set to a value less than total voting grops')
-    return None
 
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euc_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euc_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+    return vote_result
 
 
 prepare_dataset()
