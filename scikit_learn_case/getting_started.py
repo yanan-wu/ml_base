c052cda4618bbfee6a12ceec7c09a6bf45146444
yanan-wu <wuyn1760@aliyun.com>
2019-11-11 07:52:32 +0800

    2019111101_knn

--- scikit_learn_case/getting_started.py
+++ scikit_learn_case/getting_started.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+import mglearn
 import pandas as pd
 from pandas import plotting
-import mglearn
-import matplotlib.pyplot as plot
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def train_model():
@@ -19,7 +20,20 @@ def train_model():
     iris_dataframe = pd.DataFrame(X_train, columns=iris_data.feature_names)
     grr = plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
                             hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
-    plot.show()
+
+    # 训练模型
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+
+    # 预测
+    X_new = np.array([[5, 2.9, 1, 0.2]])
+    prediction = knn.predict(X_new)
+    print("Prediction: {}".format(prediction))
+    print("Predicted target name: {}".format(
+        iris_data['target_names'][prediction]))
+
+    # 模型评估
+    print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
 
 
 if __name__ == "__main__":
